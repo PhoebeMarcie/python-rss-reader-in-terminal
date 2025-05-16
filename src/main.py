09b7baaa35670e825b29ed7e1fb099c8ff3b9733
Njoroge, Mercy Phoebe Wangu M <njorogem@stanbic.com>
2025-05-16 13:57:41 +0300

    adjust rss_feed() to accept single link from user, manage error handling, format results

--- src/main.py
+++ src/main.py
@@ -5,24 +5,54 @@ import feedparser;
 
 def rss_feed():
     """
-    function uses feedparser
+    function uses feedparser to fetch and display entries from an RSS feed
+    reference tutorial:https://www.youtube.com/watch?v=ZH_Xi_1w-eg
+ 
     
     """
-    feed_link = "https://feeds.bbci.co.uk/news/rss.xml"
-    feed = feedparser.parse(feed_link)
+    #    1. Request user to enter an rss feed url and loop until a link is provided
+    feed_link = ""
+    while not feed_link:
+        feed_link = input("Enter the RSS feed URL: ")
+        if not feed_link:
+            print("Please provide an RSS feed URL.")
+        
 
-    num_links = int(input("Number of links: "))
-    print('printing here')
+
+    # 2. Request user to enter number of links they would like to view
+    
+    num_links = None
+    while num_links is None:
+        num_links_str = input("Enter the number of links you want to display: ")
+
+        if not num_links_str:
+            print("Please provide the number of links: ")
+            continue
+        try:
+            num_links = int(num_links_str)
+            if num_links <0:
+                print("Please enter a positive integer.")
+                num_links=None
+        except ValueError:
+            print("Invalid input. Please enter a whole number")
+
+    
+
+    # 3. fetch rss feed from provided link
+    feed = feedparser.parse(feed_link)
+    print('\n---- Feed Entries from: ',feed_link,'------')
 
     if not feed.entries:
-        print("No entries found in the feed.")
-        return
-
-    for link in feed.entries[:num_links]:
-        print('finding links')
-        print(link.title)
-        print(link.description)
-        print(link.link)
+        print("No Entries found in provided feed")
+    
+    for entry in feed.entries[:num_links]:
+        print('\n--- Entry ----')
+        print(f"Title: {entry.title}")
+        print(f"Description: {entry.description}")
+        print(f"Link: {entry.link}")
         print()
 
-rss_feed()
+    
+
+if __name__ == "__main__":
+    rss_feed()
